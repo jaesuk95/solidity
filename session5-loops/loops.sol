c3acd7db1e41fe09b49b2bfe1db40e58526f43b2
jaesuk95 <jaesuk95@outlook.com>
2022-03-22 07:10:53 +0900

    2nd commit

--- session5-loops/loops.sol
+++ session5-loops/loops.sol
@@ -1,8 +1,21 @@
 pragma solidity ^0.8.6;  // this tells the compiler what version we are using 
 
-
+// just adding 10 value
 contract loops {
     function count(int number) public pure returns(int){    
-        while()
+        int i = 0;
+        while(i < 10){
+            number++;
+            i++;
+        }
+        return number;
+    }
+
+    function forloop(int number) public pure returns(int){
+        for(int i = 0; i<10; i++){
+            number++;
+        }
+        return number;
     }
 }
+
